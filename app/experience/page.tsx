6da23ab69e091697354ea5b67354d055db33e037
unknown <irwinmck@gmail.com>
2024-09-28 21:36:18 -0400

    rough copy for exp

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -33,13 +33,13 @@ export default function Experience() {
               <h3>Lead Software Engineer</h3>
               <span>2023 - Pres.</span>
             </div>
-            <h4>TD Securities - Precious Metal Automation</h4>
+            <h4>TD Securities - App Engineering & Design</h4>
             <div className={styles.article}>
+              <p>Currently leading the development of Wholesale UI development.</p>
+              <p>Coordinating the migration from current cloud offering to GCP.</p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, praesentium
-                quibusdam dolorum necessitatibus adipisci iusto at, optio molestiae
-                distinctio quas fuga eligendi nesciunt numquam est in voluptates et
-                officiis consequatur!
+                Lead a team of 5 developers to rebuild legacy UI for booking precious
+                metal trades.
               </p>
             </div>
           </div>
@@ -51,13 +51,17 @@ export default function Experience() {
               <h3>Senior Software Engineer</h3>
               <span>2021</span>
             </div>
-            <h4>TD Securities - App Engineering & Design</h4>
+            <h4>TD Securities - Precious Metal Automation</h4>
             <div className={styles.article}>
+              <p>Architect Metro Auctions Platform</p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, praesentium
-                quibusdam dolorum necessitatibus adipisci iusto at, optio molestiae
-                distinctio quas fuga eligendi nesciunt numquam est in voluptates et
-                officiis consequatur!
+                Improved trader efficiency by enabling concurrent deal bookings and
+                automating aspects of the deal process like credit checking and info
+                entry.
+              </p>
+              <p>
+                Leveraged MongoDB change streaming to connect disparate systems and create
+                an event based architecture.
               </p>
             </div>
           </div>
@@ -71,12 +75,9 @@ export default function Experience() {
             </div>
             <h4>TD Securities - Internal Tooling Development</h4>
             <div className={styles.article}>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, praesentium
-                quibusdam dolorum necessitatibus adipisci iusto at, optio molestiae
-                distinctio quas fuga eligendi nesciunt numquam est in voluptates et
-                officiis consequatur!
-              </p>
+              <p>Workspace</p>
+              <p>App Starter</p>
+              <p>Internal Components Library</p>
             </div>
           </div>
           <div className={styles.avatar}>
@@ -90,10 +91,10 @@ export default function Experience() {
             <h4>TD Bank - Mainframe</h4>
             <div className={styles.article}>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, praesentium
-                quibusdam dolorum necessitatibus adipisci iusto at, optio molestiae
-                distinctio quas fuga eligendi nesciunt numquam est in voluptates et
-                officiis consequatur!
+                Had the opportunity to try out an analysis role and figured it
+                couldn&apos;t hurt. Quickly realized I enjoyed development more but I
+                think seeing the perspective from the other side of the business was
+                valuable.
               </p>
             </div>
           </div>
@@ -108,10 +109,9 @@ export default function Experience() {
             <h4>TD Bank - Business Banking</h4>
             <div className={styles.article}>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, praesentium
-                quibusdam dolorum necessitatibus adipisci iusto at, optio molestiae
-                distinctio quas fuga eligendi nesciunt numquam est in voluptates et
-                officiis consequatur!
+                This role was primarily focused on development of an Angular application
+                for data collection intended to replace a legacy process for onboarding
+                business clients.
               </p>
             </div>
           </div>
@@ -126,10 +126,8 @@ export default function Experience() {
             <h4>TD Securities - FX Trading</h4>
             <div className={styles.article}>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, praesentium
-                quibusdam dolorum necessitatibus adipisci iusto at, optio molestiae
-                distinctio quas fuga eligendi nesciunt numquam est in voluptates et
-                officiis consequatur!
+                My first job in tech! Spent most of this time learning but was able to
+                deliver some realtime charting views utilizing React and WebSockets.
               </p>
             </div>
           </div>
@@ -138,10 +136,10 @@ export default function Experience() {
           </div>
           <div className={styles.card}>
             <div className={styles.top}>
-              <h3>Student</h3>
+              <h3>Undergraduate</h3>
               <span>2015 - 2018</span>
             </div>
-            <h4>Ryerson University - Computer Science</h4>
+            <h4>Ryerson University - B.Sc. Computer Science (Honours) </h4>
           </div>
         </div>
         <footer className={styles.footer}>
